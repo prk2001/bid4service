9c095490a8637cafb7723bbf4248891db6c405f9
Kennedy Family <kennedy@Kennedys-iMac.local>
2025-12-12 13:46:39 -0500

    Update Stripe API version

--- backend/src/controllers/payment.controller.ts
+++ backend/src/controllers/payment.controller.ts
@@ -13,7 +13,7 @@ const prisma = new PrismaClient();
 
 // Initialize Stripe
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-  apiVersion: '2023-10-16',
+  apiVersion: '2025-02-24.acacia',
 });
 
 /**
